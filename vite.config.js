43986fb08f6c12137abed385245c5daf108cf638
Tejas <tejasgulati11227@gmail.com>
2025-03-26 09:53:45 +0530

    Oh

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,13 @@ export default defineConfig({
   base: '/GrameenLink/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
   }
 })
