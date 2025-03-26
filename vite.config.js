83f88a0e15ca56ec4694f8775d27b190d009139b
Tejas <tejasgulati11227@gmail.com>
2025-03-26 09:40:05 +0530

    Oh

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_TYPE || "/GrameenLink/",
+  base: process.env.VITE_BASE_TYPE || "/",
   build: {
     assetsDir: 'assets',
     rollupOptions: {
